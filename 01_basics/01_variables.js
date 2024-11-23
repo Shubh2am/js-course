f5dabb9bb3b1e1d8b3dfffe3c51a9ea52bae73f1
Shubham <87083813+Shubh2am@users.noreply.github.com>
2024-11-23 13:24:13 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const name = "Shubham"
+email = "abc.xyz.com"
+var pass = "12345"
+loginUser = true
+
+// console.log(name)
+
+function to_check_var() {
+    let email = "shubhammaheshwari475@gmail.com"
+    pass = "123"
+   
+}
+
+to_check_var()
+
+console.table([name,email,pass,loginUser])
